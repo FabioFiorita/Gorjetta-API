3932eaa0da95f71556e80aa58b8cbbd28d7b9343
Fabio Fiorita <fabiolfp@gmail.com>
2022-11-15 17:08:31 -0300

    Adicionando CORS para a API

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import gorjeta_Fuzzy
 
-app = FastAPI() #uvicorn main:app --reload
+app = FastAPI()  # uvicorn app:app --reload
 
-#Nessa rota - executa função home
-@app.post("/gorjeta")
-def home(servico: int, qualidade: int): #Faz a validação da variavel
+origins = [
+    "*",
+]
 
-    return {"gorjeta": gorjeta_Fuzzy.gorjeta(servico, qualidade)}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"],
+)
+
+# Nessa rota - executa função home
+@ app.post("/gorjeta")
+def home(servico: int, comida: int):  # Faz a validação da variavel
+    
+    return {"gorjeta": gorjeta_Fuzzy.gorjeta(servico, comida)}
